ecdc74d7a52dda3f56f685587c1fa9d3ebfcbaa3
rahul <murarilalrahul@gmail.com>
2026-09-19 12:53:32 +0530

    Improve my header section

---
+++ src/config/navigation.js
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    name: "Plastic Moulding Die",
+    href: "/products/plastic-moulding-die",
+  },
+  {
+    name: "Die Moulds",
+    href: "/products/die-moulds",
+  },
+  {
+    name: "Injection Mould",
+    href: "/products/injection-mould",
+  },
+  {
+    name: "Blow Mould",
+    href: "/products/blow-mould",
+  },
+  {
+    name: "Bottle Cap Die",
+    href: "/products/bottle-cap-die",
+  },
+  {
+    name: "Handle Die",
+    href: "/products/handle-die",
+  },
+  {
+    name: "Cap Die",
+    href: "/products/cap-die",
+  },
+];
